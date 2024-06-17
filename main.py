3fc94901c60e6953c302c6a95ac262fc8690dc1a
Hargun3 <100058136+Hargun3@users.noreply.github.com>
2024-06-16 22:01:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
+import urllib.parse
 
-from flask import Flask
+from flask import Flask , request
+
 
 app = Flask(__name__)
 app.secret_key = 'test1wsazxasmansiabddvasfv423b213b2132323'
@@ -8,7 +10,7 @@ app.secret_key = 'test1wsazxasmansiabddvasfv423b213b2132323'
 
 CLIENT_ID = 'fb9ba38989444a259af61ce8f69878ac'
 CLIENT_SECRET = 'd5a4b236ab6e4d40877817db357e873d'
-REDIRECT_URL = 'http://localhost:5000/callback'
+REDIRECT_URI = 'http://localhost:5000/callback'
 
 AUTH_URL = 'https://accounts.spotify.com/authorize'
 TOKEN_URL =  'https://accounts.spotify.com/token'
@@ -20,3 +22,20 @@ API_BASE_URL = 'https://api.spotify.com/v1/'
 def index():
     return "Welcome to my Spotify App <a> href = '/login'>Login with SPotify</a>"
 
+
+app.route('/login')
+def login():
+    scope = 'user-read-private user-read-email'
+
+    params = {
+        'client_id' : CLIENT_ID,
+        'response_type' : 'code',
+        'scope' : scope,
+        'redirect_uri' : REDIRECT_URI,
+        'show_dialog' : False  #omit later while testing
+
+    }
+
+    auth_url  = f"{AUTH_URL} ? {urllib.parse.urlencode(params)}"
+
+    return redirect(auth_url)
